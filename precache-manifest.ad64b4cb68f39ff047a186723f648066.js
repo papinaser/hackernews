ef5d9505edaca428379d9954b8f3f620cac9a12a
papinaser <papinaser@gmail.com>
2019-03-10 20:23:54 +0330

    Updates

---
+++ precache-manifest.ad64b4cb68f39ff047a186723f648066.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1f956bfc8eb1fc4a1ca",
+    "url": "/hackernews/static/js/runtime~main.f1f956bf.js"
+  },
+  {
+    "revision": "c3a090baa2bc2464cd02",
+    "url": "/hackernews/static/js/main.c3a090ba.chunk.js"
+  },
+  {
+    "revision": "ab1fa1576bc56d79d8f1",
+    "url": "/hackernews/static/js/2.ab1fa157.chunk.js"
+  },
+  {
+    "revision": "83f8390f63d8c8aa0fb9f81689c1ab34",
+    "url": "/hackernews/index.html"
+  }
+];
